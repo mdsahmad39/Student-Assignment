def0926648ad06d165b882584e3cd383f73d7726
Sarfaraz Ahmad Mahammad <45324530+mdsahmad39@users.noreply.github.com>
2021-05-31 09:42:51 +0530

    modifying get methods

--- Student-Assignment/src/main/java/com/student/app/main/StudentController.java
+++ Student-Assignment/src/main/java/com/student/app/main/StudentController.java
@@ -7,39 +7,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.student.app.dao.StudentService;
 import com.student.app.dto.Student;
 
 @RestController
+@RequestMapping("/student")
 public class StudentController {
 
 	@Autowired
 	StudentService studentService;
 
 	
-	@PostMapping("/addStudent")
+	@PostMapping("/add")
 	public String createStudentProfile(@RequestBody Student student) {
 		return studentService.createProfile(student) != null ? "Success" : "Failes";
 	}
 
-	@GetMapping("/student/all")
+	@GetMapping("/all")
 	public List<Student> allStudents() {
 		return studentService.allStudents();
 	}
 	
-	@GetMapping("/student/{rollNumber}")
+	@GetMapping("/{rollNumber}")
 	public Student findStudentByRollNumber(@PathVariable("rollNumber") long rollNumber) {
 		return studentService.findByRollNumber(rollNumber);
 	}
 	
-	@PostMapping("/updateStudent")
+	@PostMapping("/update")
 	public String updateStudent(@RequestBody Student student) {
 		return studentService.updateProfile(student) != null ? "Success" : "Failes";
 	}
 	
-	@PostMapping("/deleteStudent")
+	@PostMapping("/delete")
 	public void deleteStudent(@RequestBody Student student) {
 		studentService.deleteProfile(student);
 	}
